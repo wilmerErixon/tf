a16bd91da63b257fc9ac322cca1044c8412aa90c
wilmerErixon <118274983+wilmerErixon@users.noreply.github.com>
2024-02-28 19:44:40 +0100

    Lektion vecka 9
    lagade logga in funktionen och förbättrade den samt skapade en logga ut funktion.

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ require 'bcrypt'
 
 enable :sessions
 
+helpers do
+  def logged_in?
+    !session[:id].nil? && !session[:username].nil?
+  end
+end
+
 get('/')  do
     slim(:start)
 end 
@@ -18,29 +24,47 @@ get('/showlogin')  do
   slim(:login)
 end 
 
+get('/wrongusername') do
+  slim(:wrongUsername)
+end
+
+get('/wrongpassword') do
+  slim(:wrongpassword)
+end
+
 post('/login') do
   username = params[:username]
   password = params[:password]
-  db = SQLite3::Database.new('db/todo2021.db')
+  db = SQLite3::Database.new('db/bookies.db')
   db.results_as_hash = true
   result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+  if result == nil 
+    redirect('/wrongusername')
+  end
   pwdigest = result["pwdigest"]
   id = result["id"]
-  
+
   if BCrypt::Password.new(pwdigest) == password
     session[:id] = id
+    session[:username] = username
+    p session[:id]
     redirect('/bookies')
   else
-    "Wrong password"
+    redirect('/wrongpassword')
   end
+end
+
+get('/logout') do
+  session.clear
+  redirect('/')
 end
 
 get('/bookies')  do
   id = session[:id].to_i
-  db = SQLite3::Database.new('db/todo2021.db')
+  db = SQLite3::Database.new('db/bookies.db')
   db.results_as_hash = true
-  result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
-  p "Alla todos från result #{result}"
+  result = db.execute("SELECT * FROM users WHERE id = ?",id)
+  p "Alla bookies från result #{result}"
   slim(:"bookies/index",locals:{bookies:result})
 end 
 
@@ -51,7 +75,7 @@ post('/users/new') do
 
   if password == password_confirm
     password_digest = BCrypt::Password.create(password)
-    db = SQLite3::Database.new('db/todo2021.db')
+    db = SQLite3::Database.new('db/bookies.db')
     db.execute('INSERT INTO users (username,pwdigest) VALUES (?,?)',username,password_digest)
     redirect('/')
   else
